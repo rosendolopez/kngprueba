aa2c72065d6c5ca67cef2327928ecc1f13f0bee5
rosendolopez <rosendo.pez@gmail.com>
2019-02-22 00:35:48 -0600

    rlopez se modifica clase Prueba2

--- kngprueba/src/main/java/mx/com/prueba/Prueba2.java
+++ kngprueba/src/main/java/mx/com/prueba/Prueba2.java
@@ -1,5 +1,24 @@
 package mx.com.prueba;
 
 public class Prueba2 {
+	
+	private Integer id;
+	private String nombre;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
 
 }
